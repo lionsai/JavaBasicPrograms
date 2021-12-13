b799c380540f0a302ffbf869be180f4eedb730ec
lionsai <goripartisaikrishna@gmail.com>
2021-12-13 22:44:06 +0530

    Added Factors

--- BasicPrograms.java
+++ BasicPrograms.java
@@ -1,19 +1,22 @@
 package JavaBasicPrograms;
 import java.util.Scanner;
 
-public class PowerOfTwo {
+public class PrimeFactorization {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number:");
-		int N = sc.nextInt();
-		int R = 1;
-		for(int i = 1; i <= N; i++) {
-			R = R*2;
-			System.out.println(R);
-		}
+		int a = sc.nextInt();
 		
+		for(int i = 2; i < a; i++) {
+			while(a%i == 0) {
+	            System.out.println(i+" ");
+	            a = a/i;
+			}
+		}
+		if(a>2) {
+			System.out.println(a);
+		}
 	}
-
 }
 
