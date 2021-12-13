17a1902a91656a5c9baf67d8784ca346344655ce
lionsai <goripartisaikrishna@gmail.com>
2021-12-13 22:54:38 +0530

    Added swap

--- BasicPrograms.java
+++ BasicPrograms.java
@@ -1,22 +1,22 @@
 package JavaBasicPrograms;
 import java.util.Scanner;
 
-public class PrimeFactorization {
+public class SwapTwoNumbers {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number:");
+		System.out.println("Enter the First number: ");
 		int a = sc.nextInt();
-		
-		for(int i = 2; i < a; i++) {
-			while(a%i == 0) {
-	            System.out.println(i+" ");
-	            a = a/i;
-			}
-		}
-		if(a>2) {
-			System.out.println(a);
-		}
+		System.out.println("Enter the second number: ");
+		int b = sc.nextInt();
+		int temp = 0;
+		System.out.println(a);
+		System.out.println(b);
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After swap: "+a);
+		System.out.println("After swap: "+b);
 	}
 }
 
